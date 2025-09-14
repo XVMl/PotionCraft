bc434ed828ed46507f465aa07fed12a24c941a69
3 <3120029259@qq.com>
2025-09-14 23:54:28 +0800

    UI资源

---
+++ Content/Items/BaseCustomMaterials.cs
@@ -0,0 +1,20 @@
+using Terraria.ModLoader;
+
+namespace PotionCraft.Content.Items;
+
+public class BaseCustomMaterials:ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 20;
+        Item.height = 32;
+        
+    }
+    
+    
+}
